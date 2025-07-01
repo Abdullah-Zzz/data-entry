e3a83b21f80c6d018ac7729ef6b8c1a38ba5f386
abdullah-zzz <ak0344494@gmail.com>
2025-07-01 21:36:30 +0500

    changed url

--- src/components/QRCodePage.jsx
+++ src/components/QRCodePage.jsx
@@ -1,20 +1,30 @@
-// src/components/QRCodePage.jsx
 import React from "react";
 import { useParams } from "react-router-dom";
 import QRCode from "react-qr-code";
 
 function QRCodePage() {
   const { name } = useParams();
-  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
   const retrieveURL = `https://online.csgb.gov.ct.tr.online-csgb.online/CIP/BasvuruAnaQRKODValidation/Validate?Ne=onizinbelgepdf&Pkey=927653&BruteForceKey=${name}`;
 
   return (
     <div style={{ textAlign: "center", padding: "40px" }}>
-      <h2>QR Code for {name}'s Data</h2>
       <div style={{ background: "white", padding: "16px", display: "inline-block" }}>
         <QRCode value={retrieveURL} size={180} />
       </div>
-      <p style={{ marginTop: "20px" }}>{retrieveURL}</p>
+
+      <div
+        style={{
+          marginTop: "20px",
+          padding: "10px",
+          maxWidth: "90%",
+          wordBreak: "break-all",
+          marginInline: "auto",
+          fontSize: "14px",
+          lineHeight: "1.4",
+        }}
+      >
+        {retrieveURL}
+      </div>
     </div>
   );
 }
