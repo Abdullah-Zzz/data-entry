53674e381616adab155a000db5be716f8734f489
abdullah-zzz <ak0344494@gmail.com>
2025-07-02 20:39:52 +0500

    changes

--- src/components/RetrievePage.jsx
+++ src/components/RetrievePage.jsx
@@ -41,61 +41,84 @@ function RetrievePage() {
   if (!data) return <p style={{ textAlign: "center" }}>{t("Loading...")}</p>;
 
   return (
-    <div style={{ cursor: 'auto', display: 'flex', flexDirection: 'column', minHeight: '100vh', margin: 0, padding: 0, fontSize: '14px', fontWeight: 300, fontFamily: 'Poppins', lineHeight: '19.5px', color: 'rgb(33, 37, 41)', textAlign: 'left', backgroundColor: '#fff' }}>
-      <div>
-        <div style={{ display: 'flex', flexWrap: 'wrap', marginRight: '-15px', marginLeft: '-15px' }}>
+    <div
+      style={{
+        cursor: "auto",
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+        margin: 0,
+        padding: 0,
+        fontSize: "14px",
+        fontWeight: 300,
+        fontFamily: "Poppins",
+        lineHeight: "1.6",
+        color: "rgb(33, 37, 41)",
+        backgroundColor: "#fff",
+      }}
+    >
+      <div style={{ padding: "0", maxWidth: "100%" }}>
+        <div style={{ textAlign: "center", marginBottom: "20px" }}>
+          <img width="100" height="100" src="/logodata.jpg" />
+          <br />
+          <img src="/logo_default_dark.png" style={{ marginTop: "10px" }} />
+        </div>
 
-          <div style={{ flex: '0 0 100%', maxWidth: '100%', paddingRight: '15px', paddingLeft: '15px' }}>
-            <p align="center">
-              <img align="middle" width="100" height="100" src="/logodata.jpg" />
-            </p>
-            <p align="center">
-              <img align="middle" src="/logo_default_dark.png" />
-            </p>
+        <div
+          style={{
+            boxShadow: "rgba(69, 65, 78, 0.08) 0px 1px 15px 1px",
+            backgroundColor: "#fff",
+            marginBottom: "28.6px",
+            padding: "28.6px",
+            textAlign: "left",
+          }}
+        >
+          <h3
+            style={{
+              color: "rgb(87, 89, 98)",
+              fontSize: "16px",
+              fontWeight: 500,
+              fontFamily: "Roboto, sans-serif",
+              marginBottom: "20px",
+            }}
+          >
+            Kare Kod Validasyonu (QR Code Validation)
+          </h3>
+
+          <h2
+            style={{
+              color: "rgb(52, 191, 163)",
+              fontSize: "22px",
+              fontWeight: 500,
+              marginBottom: "15px",
+            }}
+          >
+            Bilgiler
+          </h2>
+
+          <div>
+            <img
+              width="90"
+              height="120"
+              style={{ imageOrientation: "from-image", marginBottom: "15px" }}
+              src={data.image}
+              alt="Uploaded"
+            />
           </div>
 
-          <div style={{ flex: '0 0 25%', maxWidth: '25%', paddingRight: '15px', paddingLeft: '15px' }}></div>
-
-          <div style={{ flex: '0 0 50%', maxWidth: '50%', paddingRight: '15px', paddingLeft: '15px' }}>
-            <div style={{ boxShadow: 'rgba(69, 65, 78, 0.08) 0px 1px 15px 1px', backgroundColor: '#fff', marginBottom: '28.6px' }}>
-              <div style={{ borderBottom: '1px solid rgb(235, 237, 242)', display: 'flex', justifyContent: 'space-between', padding: '0px 28.6px', height: '66.2969px' }}>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <h3 style={{ color: 'rgb(87, 89, 98)', display: 'flex', alignItems: 'center', fontSize: '15px', fontWeight: 500, fontFamily: 'Roboto,sans-serif', margin: 0 }}>
-                    Kare Kod Validasyonu (QR Code Validation)
-                  </h3>
-                </div>
-              </div>
-
-              <div className="data-text" style={{ color: 'rgb(17, 6, 6)', padding: '28.6px' }}>
-                <h2 style={{ color: 'rgb(52, 191, 163)', fontSize: '28px', marginBottom: '6.5px', fontFamily: 'Poppins', fontWeight: 500 }}>Bilgiler</h2>
-                <table style={{ borderCollapse: 'collapse' }}>
-                  <tbody>
-                    <tr>
-                      <td></td>
-                      <td>
-                        <img width="90" height="120" style={{ imageOrientation: 'from-image' }} src={data.image} />
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-                Başvuru No: <b>{data.applicationNo}</b><br />
-                Başvuru Tipi: <b>Ön İzin Başvurusu</b><br />
-                Çalışan (İşçi) Ad Soyad: <b>{data.Name}</b><br />
-                Çalışan (İşçi) Pasaport No: <b>{data.passportNo}</b><br />
-                Çalışan (İşçi) TC Kimlik No: <b>{data.TRidentityNumber}</b><br />
-                Çalışan(İşçi) Anne Adı: <b>{data.MothersName}</b><br />
-                Çalışan(İşçi) Baba Adı: <b>{data.fathersName}</b><br /><br />
-                Belge Geçerlilik Başlangıç Tarihi: <b>{data.documentValidityStartDate}</b><br />
-                Belge Geçerlilik Bitiş Tarihi: <b>{data.documentValidityEndDate}</b><br />
-                Onay Durumu: <b>{data.approvalStatus ? 'Evet' : 'Hayır'}</b><br />
-                Onay Tarihi: <b>{data.approvalDate}</b><br />
-              </div>
-
-            </div>
+          <div>
+            Başvuru No: <b>{data.applicationNo}</b><br />
+            Başvuru Tipi: <b>Ön İzin Başvurusu</b><br />
+            Çalışan (İşçi) Ad Soyad: <b>{data.Name}</b><br />
+            Çalışan (İşçi) Pasaport No: <b>{data.passportNo}</b><br />
+            Çalışan (İşçi) TC Kimlik No: <b>{data.TRidentityNumber}</b><br />
+            Çalışan(İşçi) Anne Adı: <b>{data.MothersName}</b><br />
+            Çalışan(İşçi) Baba Adı: <b>{data.fathersName}</b><br /><br />
+            Belge Geçerlilik Başlangıç Tarihi: <b>{data.documentValidityStartDate}</b><br />
+            Belge Geçerlilik Bitiş Tarihi: <b>{data.documentValidityEndDate}</b><br />
+            Onay Durumu: <b>{data.approvalStatus ? 'Evet' : 'Hayır'}</b><br />
+            Onay Tarihi: <b>{data.approvalDate}</b><br />
           </div>
-
-          <div style={{ flex: '0 0 25%', maxWidth: '25%', paddingRight: '15px', paddingLeft: '15px' }}></div>
-
         </div>
       </div>
     </div>
