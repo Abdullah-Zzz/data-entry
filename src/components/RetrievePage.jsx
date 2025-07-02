5843a8df0df7d7f69ee0b564c7376a2507e8195b
abdullah-zzz <ak0344494@gmail.com>
2025-07-02 20:26:17 +0500

    changes

--- src/components/RetrievePage.jsx
+++ src/components/RetrievePage.jsx
@@ -41,65 +41,108 @@ function RetrievePage() {
   if (!data) return <p style={{ textAlign: "center" }}>{t("Loading...")}</p>;
 
   return (
-    <div style={{ cursor: 'auto', display: 'flex', flexDirection: 'column', minHeight: '100vh', margin: 0, padding: 0, fontSize: '14px', fontWeight: 300, fontFamily: 'Poppins', lineHeight: '19.5px', color: 'rgb(33, 37, 41)', textAlign: 'left', backgroundColor: '#fff',}}>
+    <div style={{
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      minHeight: '100vh',
+      margin: 0,
+      padding: 0,
+      fontSize: '14px',
+      fontWeight: 300,
+      fontFamily: 'Poppins',
+      lineHeight: '19.5px',
+      color: 'rgb(33, 37, 41)',
+      backgroundColor: '#fff',
+    }}>
       <div>
         <div style={{ display: 'flex', flexWrap: 'wrap', marginRight: '-15px', marginLeft: '-15px' }}>
 
-          <div style={{ flex: '0 0 100%', maxWidth: '100%', paddingRight: '15px', paddingLeft: '15px' }}>
-            <p align="center">
+          {/* Centered Logos */}
+          <div style={{ width: '100%', paddingRight: '15px', paddingLeft: '15px', textAlign: 'center' }}>
+            <p>
               <img align="middle" width="100" height="100" src="/logodata.jpg" />
             </p>
-            <p align="center">
+            <p>
               <img align="middle" src="/logo_default_dark.png" />
             </p>
           </div>
 
-          <div style={{ flex: '0 0 25%', maxWidth: '25%', paddingRight: '15px', paddingLeft: '15px' }}></div>
-
-          <div style={{ flex: '0 0 50%', maxWidth: '50%', paddingRight: '15px', paddingLeft: '15px' }}>
-            <div style={{ boxShadow: 'rgba(69, 65, 78, 0.08) 0px 1px 15px 1px', backgroundColor: '#fff', marginBottom: '28.6px' }}>
-              <div style={{ borderBottom: '1px solid rgb(235, 237, 242)', display: 'flex', justifyContent: 'space-between', padding: '0px 28.6px', height: '66.2969px' }}>
+          {/* Content */}
+          <div style={{ width: '100%', padding: '0 15px' }}>
+            <div style={{
+              boxShadow: 'rgba(69, 65, 78, 0.08) 0px 1px 15px 1px',
+              backgroundColor: '#fff',
+              marginBottom: '28.6px',
+            }}>
+              <div style={{
+                borderBottom: '1px solid rgb(235, 237, 242)',
+                display: 'flex',
+                justifyContent: 'space-between',
+                padding: '0px 28.6px',
+                height: '66.2969px'
+              }}>
                 <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <h3 style={{ color: 'rgb(87, 89, 98)', display: 'flex', alignItems: 'center', fontSize: '15px', fontWeight: 500, fontFamily: 'Roboto,sans-serif', margin: 0 }}>
+                  <h3 style={{
+                    color: 'rgb(87, 89, 98)',
+                    display: 'flex',
+                    alignItems: 'center',
+                    fontSize: '15px',
+                    fontWeight: 500,
+                    fontFamily: 'Roboto,sans-serif',
+                    margin: 0
+                  }}>
                     Kare Kod Validasyonu (QR Code Validation)
                   </h3>
                 </div>
               </div>
 
               <div style={{ color: 'rgb(17, 6, 6)', padding: '28.6px' }}>
-                <h2 style={{ color: 'rgb(52, 191, 163)', fontSize: '28px', marginBottom: '6.5px', fontFamily: 'Poppins', fontWeight: 500 }}>Bilgiler</h2>
+                <h2 style={{
+                  color: 'rgb(52, 191, 163)',
+                  fontSize: '28px',
+                  marginBottom: '6.5px',
+                  fontFamily: 'Poppins',
+                  fontWeight: 500
+                }}>
+                  Bilgiler
+                </h2>
+
                 <table style={{ borderCollapse: 'collapse' }}>
                   <tbody>
                     <tr>
                       <td></td>
                       <td>
-                        <img width="90" height="120" style={{ imageOrientation: 'from-image' }} src={data.image} />
+                        <img
+                          width="90"
+                          height="120"
+                          style={{ imageOrientation: 'from-image' }}
+                          src={data.image}
+                        />
                       </td>
                     </tr>
                   </tbody>
                 </table>
-                Başvuru No:<b>{data.applicationNo}</b><br />
-                Başvuru Tipi:<b>Ön İzin Başvurusu</b><br />
-                Çalışan (İşçi) Ad Soyad:<b>{data.Name}</b><br />
-                Çalışan (İşçi) Pasaport No:<b>{data.passportNo}</b><br />
-                Çalışan (İşçi) TC Kimlik No:<b>{data.TRidentityNumber}</b><br />
-                Çalışan(İşçi) Anne Adı:<b>{data.MothersName}</b><br />
-                Çalışan(İşçi) Baba Adı:<b>{data.fathersName}</b><br /><br />
-                Belge Geçerlilik Başlangıç Tarihi:<b>{data.documentValidityStartDate}</b><br />
-                Belge Geçerlilik Bitiş Tarihi:<b>{data.documentValidityEndDate}</b><br />
-                Onay Durumu:<b>{data.approvalStatus ? 'Evet' : 'Hayır'}</b><br />
-                Onay Tarihi:<b>{data.approvalDate}</b><br />
+
+                Başvuru No: <b>{data.applicationNo}</b><br />
+                Başvuru Tipi: <b>Ön İzin Başvurusu</b><br />
+                Çalışan (İşçi) Ad Soyad: <b>{data.Name}</b><br />
+                Çalışan (İşçi) Pasaport No: <b>{data.passportNo}</b><br />
+                Çalışan (İşçi) TC Kimlik No: <b>{data.TRidentityNumber}</b><br />
+                Çalışan(İşçi) Anne Adı: <b>{data.MothersName}</b><br />
+                Çalışan(İşçi) Baba Adı: <b>{data.fathersName}</b><br /><br />
+                Belge Geçerlilik Başlangıç Tarihi: <b>{data.documentValidityStartDate}</b><br />
+                Belge Geçerlilik Bitiş Tarihi: <b>{data.documentValidityEndDate}</b><br />
+                Onay Durumu: <b>{data.approvalStatus ? 'Evet' : 'Hayır'}</b><br />
+                Onay Tarihi: <b>{data.approvalDate}</b><br />
               </div>
 
             </div>
           </div>
-
-          <div style={{ flex: '0 0 25%', maxWidth: '25%', paddingRight: '15px', paddingLeft: '15px' }}></div>
-
-        </div>  
+        </div>
       </div>
     </div>
-  );  
+  );
 }
 
 export default RetrievePage;
